79ab1aa017c8e1a0aa4b867ccaf8e53198b0743e
jkian <slyrics443@gmail.com>
2026-01-21 16:56:06 +0800

    added side bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,43 @@
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
 /*=============== SHOW MENU ===============*/
 
-/*============== MENU HIDDEN ===============*/
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
+/*============== MENU HIDDEN ===============*/
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 /*=============== REMOVE MENU MOBILE ===============*/
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+
+  navMenu.classList.remove("show-menu");
+}
+
+navLinks.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 /*=============== TESTIMONIAL SWIPER ===============*/
-var swiper=new Swiper(".testimonail-wrapper",{
-    loop:'true',
-    pagination:{
-        el:'.swiper-pagination',
-        clickable:'true'
-    }
-})
+var swiper = new Swiper(".testimonail-wrapper", {
+  loop: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: "true",
+  },
+});
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== PORTFOLIO ITEM FILTER ===============*/
