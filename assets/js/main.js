e1787aa4c9c636fd12edace9692fb7535435dce4
jkian <slyrics443@gmail.com>
2026-01-22 16:52:21 +0800

    added font customazation

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,19 +54,58 @@ var swiper = new Swiper(".testimonail-wrapper", {
 
 const theme = document.querySelector("#theme-button");
 const themeModal = document.querySelector(".customize-theme");
+const fontSizes = document.querySelectorAll(".choose-size span");
+const colorPalette=document.querySelectorAll('.choose-color span')
 
 const openThemeModal = () => {
   themeModal.classList.add("show");
-}
-const closeThemeModal=(e)=>{
-  if(e.target.classList.contains('customize-theme')){
-    themeModal.classList.remove('show')
+};
+const closeThemeModal = (e) => {
+  if (e.target.classList.contains("customize-theme")) {
+    themeModal.classList.remove("show");
   }
-}
+};
 theme.addEventListener("click", openThemeModal);
 themeModal.addEventListener("click", closeThemeModal);
+
+
+
 /*===== FONTS =====*/
 
+const removeSizeSelector=()=>{
+  fontSizes.forEach(size=>{
+    size.classList.remove('active')
+  })
+}
+
+fontSizes.forEach((size) => {
+  size.addEventListener("click", () => {
+
+    removeSizeSelector();
+
+    let fontSize;
+
+
+    size.classList.toggle('active'); //toggles the 'active' class on the clicked element
+
+
+    if (size.classList.contains("font-size-1")) {
+      fontSize = "12px";
+    } else if (size.classList.contains("font-size-2")) {
+      fontSize = "14px";
+    } else if (size.classList.contains("font-size-3")) {
+      fontSize = "16px";
+    } else if (size.classList.contains("font-size-4")) {
+      fontSize = "18px";
+    }
+
+    document.querySelector("html").style.fontSize = fontSize;
+
+  });
+});
+
 /*===== PRIMARY COLORS =====*/
+
+
 
 /*===== THEME BACKGROUNDS =====*/
