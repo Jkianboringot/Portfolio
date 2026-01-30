66ec0a8d2a6a7ab028f025a725f96ec0b84e990a
jkian <slyrics443@gmail.com>
2026-01-30 15:12:25 +0800

    slider works now it adds active class

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,22 @@ if (navToggle) {
   });
 }
 
+
+/*=============== Light/Dark Mode ===============*/
+
+const slider = document.getElementById("slider");
+const slideInput = document.getElementById("slider-input");
+
+slideInput.addEventListener("change", () => {
+  // Add 'active' if checked, remove if unchecked
+  slider.classList.toggle("active", slideInput.checked);
+});
+
+
+
+
+
+
 /*============== MENU HIDDEN ===============*/
 if (navClose) {
   navClose.addEventListener("click", () => {
@@ -47,188 +63,187 @@ var swiper = new Swiper(".testimonail-wrapper", {
   },
 });
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]');
+const sections = document.querySelectorAll("section[id]");
 
-window.addEventListener('scroll', navHighlighter);
+window.addEventListener("scroll", navHighlighter);
 
 function navHighlighter() {
   let scrollY = window.pageYOffset;
 
-  sections.forEach(current => {
+  sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 60; // adjust buffer for header
-    const sectionId = current.getAttribute('id');
+    const sectionId = current.getAttribute("id");
 
-    const navLink = document.querySelector('.nav-menu a[href*="'+sectionId+'"]');
+    const navLink = document.querySelector(
+      '.nav-menu a[href*="' + sectionId + '"]',
+    );
     if (!navLink) return;
 
-    if(scrollY >= sectionTop && scrollY < sectionTop + sectionHeight){
-      navLink.classList.add('active-link');
+    if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+      navLink.classList.add("active-link");
     } else {
-      navLink.classList.remove('active-link');
+      navLink.classList.remove("active-link");
     }
   });
 }
 
+// /*=============== Project ITEM FILTER ===============*/
+// const filterContainer = document.querySelector(".project-filter-inner");
+// const filterBtns = filterContainer.querySelectorAll("button");
+// const projectItems = document.querySelectorAll(".project-item");
+
+// filterBtns.forEach((btn) => {
+//   btn.addEventListener("click", function () {
+//     // Remove active from all buttons
+//     filterBtns.forEach((b) => b.classList.remove("active"));
+
+//     // Add active to clicked button
+//     this.classList.add("active");
+
+//     const filterValue = this.getAttribute("data-filter");
+
+//     projectItems.forEach((item) => {
+//       const category = item.getAttribute("data-category");
+
+//       if (filterValue === "all" || filterValue === category) {
+//         item.style.display = "block"; // Show
+//       } else {
+//         item.style.display = "none"; // Hide
+//       }
+//     });
+//   });
+// });
+
+// // /*=============== THEME/DISPLAY CUSTOMIZATION ===============*/
+
+// // const theme = document.querySelector("#theme-button");
+// // const themeModal = document.querySelector(".customize-theme");
+
+// // const fontSizes = document.querySelectorAll(".choose-size span");
+
+// // const colorPalette = document.querySelectorAll(".choose-color span");
+// // var root = document.querySelector(":root");
+
+// // const Bg1 = document.querySelector(".bg-1");
+// // const Bg2 = document.querySelector(".bg-2");
+// // const Bg3 = document.querySelector(".bg-3");
+
+// // const openThemeModal = () => {
+// //   themeModal.classList.add("show");
+// // };
+// // const closeThemeModal = (e) => {
+// //   if (e.target.classList.contains("customize-theme")) {
+// //     themeModal.classList.remove("show");
+// //   }
+// // };
+// // theme.addEventListener("click", openThemeModal);
+// // themeModal.addEventListener("click", closeThemeModal);
+
+// // /*===== FONTS =====*/
+
+// // // const removeSizeSelector = () => {
+// // //   fontSizes.forEach((size) => {
+// // //     size.classList.remove("active");
+// // //   });
+// // // };
+
+// // // fontSizes.forEach((size) => {
+// // //   size.addEventListener("click", () => {
+// // //     removeSizeSelector();
+
+// // //     let fontSize;
+
+// // //     size.classList.toggle("active"); //toggles the 'active' class on the clicked element
+
+// // //     if (size.classList.contains("font-size-1")) {
+// // //       fontSize = "12px";
+// // //     } else if (size.classList.contains("font-size-2")) {
+// // //       fontSize = "14px";
+// // //     } else if (size.classList.contains("font-size-3")) {
+// // //       fontSize = "16px";
+// // //     } else if (size.classList.contains("font-size-4")) {
+// // //       fontSize = "18px";
+// // //     }
+
+// // //     document.querySelector("html").style.fontSize = fontSize;
+// // //   });
+// // // });
+
+// /*===== PRIMARY COLORS =====*/
+
+// const removeColorSelector = () => {
+//   colorPalette.forEach((color) => {
+//     color.classList.remove("active");
+//   });
+// };
+
+// colorPalette.forEach((color) => {
+//   color.addEventListener("click", () => {
+//     removeColorSelector();
+
+//     let colorhue;
+
+//     color.classList.toggle("active"); //toggles the 'active' class on the clicked element
+
+//     if (color.classList.contains("color-1")) {
+//       colorhue = "252";
+//     } else if (color.classList.contains("color-2")) {
+//       colorhue = "52";
+//     } else if (color.classList.contains("color-3")) {
+//       colorhue = "352";
+//     } else if (color.classList.contains("color-4")) {
+//       colorhue = "152";
+//     } else if (color.classList.contains("color-5")) {
+//       colorhue = "202";
+//     }
+
+//     root.style.setProperty("--primary-color-hue", colorhue);
+//   });
+// });
+
+// /*===== THEME BACKGROUNDS =====*/
+// let lightColorLightness;
+// let whiteColorLightness;
+// let darkColorLightness;
+
+// const changeBG = () => {
+//   root.style.setProperty("--light-color-lightness", lightColorLightness);
+//   root.style.setProperty("--white-color-lightness", whiteColorLightness);
+//   root.style.setProperty("--dark-color-lightness", darkColorLightness);
+// };
+
+// Bg1.addEventListener("click", () => {
+//   Bg1.classList.add("active");
+
+//   Bg2.classList.remove("active");
+//   Bg3.classList.remove("active");
+
+//   window.location.reload();
+// });
+
+// Bg2.addEventListener("click", () => {
+//   darkColorLightness = "95%";
+//   whiteColorLightness = "20%";
+//   lightColorLightness = "15%";
+
+//   Bg2.classList.add("active");
+
+//   Bg1.classList.remove("active");
+//   Bg3.classList.remove("active");
+//   changeBG();
+// });
+
+// Bg3.addEventListener("click", () => {
+//   darkColorLightness = "95%";
+//   whiteColorLightness = "10%";
+//   lightColorLightness = "0%";
+
+//   Bg3.classList.add("active");
+
+//   Bg2.classList.remove("active");
+//   Bg1.classList.remove("active");
+//   changeBG();
+// });
 
 
-
-
-/*=============== Project ITEM FILTER ===============*/
-const filterContainer = document.querySelector(".project-filter-inner");
-const filterBtns = filterContainer.querySelectorAll("button");
-const projectItems = document.querySelectorAll(".project-item");
-
-filterBtns.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    // Remove active from all buttons
-    filterBtns.forEach((b) => b.classList.remove("active"));
-
-    // Add active to clicked button
-    this.classList.add("active");
-
-    const filterValue = this.getAttribute("data-filter");
-
-    projectItems.forEach((item) => {
-      const category = item.getAttribute("data-category");
-
-      if (filterValue === "all" || filterValue === category) {
-        item.style.display = "block"; // Show
-      } else {
-        item.style.display = "none"; // Hide
-      }
-    });
-  });
-});
-
-
-/*=============== THEME/DISPLAY CUSTOMIZATION ===============*/
-
-const theme = document.querySelector("#theme-button");
-const themeModal = document.querySelector(".customize-theme");
-
-const fontSizes = document.querySelectorAll(".choose-size span");
-
-const colorPalette = document.querySelectorAll(".choose-color span");
-var root = document.querySelector(":root");
-
-const Bg1 = document.querySelector(".bg-1");
-const Bg2 = document.querySelector(".bg-2");
-const Bg3 = document.querySelector(".bg-3");
-
-const openThemeModal = () => {
-  themeModal.classList.add("show");
-};
-const closeThemeModal = (e) => {
-  if (e.target.classList.contains("customize-theme")) {
-    themeModal.classList.remove("show");
-  }
-};
-theme.addEventListener("click", openThemeModal);
-themeModal.addEventListener("click", closeThemeModal);
-
-/*===== FONTS =====*/
-
-const removeSizeSelector = () => {
-  fontSizes.forEach((size) => {
-    size.classList.remove("active");
-  });
-};
-
-fontSizes.forEach((size) => {
-  size.addEventListener("click", () => {
-    removeSizeSelector();
-
-    let fontSize;
-
-    size.classList.toggle("active"); //toggles the 'active' class on the clicked element
-
-    if (size.classList.contains("font-size-1")) {
-      fontSize = "12px";
-    } else if (size.classList.contains("font-size-2")) {
-      fontSize = "14px";
-    } else if (size.classList.contains("font-size-3")) {
-      fontSize = "16px";
-    } else if (size.classList.contains("font-size-4")) {
-      fontSize = "18px";
-    }
-
-    document.querySelector("html").style.fontSize = fontSize;
-  });
-});
-
-/*===== PRIMARY COLORS =====*/
-
-const removeColorSelector = () => {
-  colorPalette.forEach((color) => {
-    color.classList.remove("active");
-  });
-};
-
-colorPalette.forEach((color) => {
-  color.addEventListener("click", () => {
-    removeColorSelector();
-
-    let colorhue;
-
-    color.classList.toggle("active"); //toggles the 'active' class on the clicked element
-
-    if (color.classList.contains("color-1")) {
-      colorhue = "252";
-    } else if (color.classList.contains("color-2")) {
-      colorhue = "52";
-    } else if (color.classList.contains("color-3")) {
-      colorhue = "352";
-    } else if (color.classList.contains("color-4")) {
-      colorhue = "152";
-    } else if (color.classList.contains("color-5")) {
-      colorhue = "202";
-    }
-
-    root.style.setProperty("--primary-color-hue", colorhue);
-  });
-});
-
-/*===== THEME BACKGROUNDS =====*/
-let lightColorLightness;
-let whiteColorLightness;
-let darkColorLightness;
-
-const changeBG = () => {
-  root.style.setProperty("--light-color-lightness", lightColorLightness);
-  root.style.setProperty("--white-color-lightness", whiteColorLightness);
-  root.style.setProperty("--dark-color-lightness", darkColorLightness);
-};
-
-Bg1.addEventListener("click", () => {
-  Bg1.classList.add("active");
-
-  Bg2.classList.remove("active");
-  Bg3.classList.remove("active");
-
-  window.location.reload();
-});
-
-Bg2.addEventListener("click", () => {
-  darkColorLightness = "95%";
-  whiteColorLightness = "20%";
-  lightColorLightness = "15%";
-
-  Bg2.classList.add("active");
-
-  Bg1.classList.remove("active");
-  Bg3.classList.remove("active");
-  changeBG();
-});
-
-Bg3.addEventListener("click", () => {
-  darkColorLightness = "95%";
-  whiteColorLightness = "10%";
-  lightColorLightness = "0%";
-
-  Bg3.classList.add("active");
-
-  Bg2.classList.remove("active");
-  Bg1.classList.remove("active");
-  changeBG();
-});
